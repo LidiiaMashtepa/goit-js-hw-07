53c145c67275717f3656e886d13ebe7363556228
Lida <108767144+LidiiaMashtepa@users.noreply.github.com>
2023-01-12 00:39:03 +0100

    add

---
+++ src/js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createCard(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== `IMG`) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+        <img src="${evt.target.dataset.source}" width="800" height="600">`);
+
+  instance.show();
+}
+function onEscapeButton(evt) {
+  if (evt.key === "Escape") {
+    console.log("blin");
+    // instance.close();
+  }
+}
+function createCard(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+    })
+    .join("");
+}
---
+++ src/js/02-lightbox.js
@@ -0,0 +1,37 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createCard(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== `IMG`) {
+    return;
+  }
+}
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createCard(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+    })
+    .join("");
+}
